b293f3dea70c62abe8f06b787c96627dc42b1bc9
ybark <kevindoughlife.com>
2020-09-24 23:38:07 -0400

    kahve eklendi

--- src/test/java/Cay.java
+++ src/test/java/Cay.java
@@ -2,5 +2,7 @@ public class Cay {
 
     public static void main(String[] args) {
         System.out.println("Agalar Cay icelim");
+
+        System.out.println("Agalar kahve de icelim");
     }
 }
